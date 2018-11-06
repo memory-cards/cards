13cbef47156ab82ead8a183ae878f8e78767c0a4
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-06 14:12:34 +0400

    Add json5 formatter

--- .scripts/formatJSON5docs.js
+++ .scripts/formatJSON5docs.js
@@ -2,6 +2,9 @@ const glob = require('glob');
 const fs = require('fs');
 const json5 = require('json5');
 
+const cmd = process.argv.join(' ');
+const validate = cmd.includes('--validate');
+
 const json5files = glob.sync('./**/*.json5');
 let counter = 0;
 json5files.forEach((fileName) => {
@@ -9,9 +12,15 @@ json5files.forEach((fileName) => {
   const data = json5.parse(content);
   const newContent = json5.stringify(data, null, 2);
   if (newContent !== content) {
-    fs.writeFileSync(fileName, newContent);
+    if(!validate) {
+       fs.writeFileSync(fileName, newContent);
+    }
     counter ++;
   }
 });
 
-console.log(`${counter} json5-file(s) reformatted`);
+console.log(`${counter} json5-file(s) ${validate ? 'should be' : ''} reformatted`);
+
+if (validate && counter) {
+  process.exit(1);
+}
