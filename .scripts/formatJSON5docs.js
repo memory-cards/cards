fc1a42ecb03adae16091ee048a87a6f7617045ee
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-07 20:36:04 +0400

    Reformat script

--- .scripts/formatJSON5docs.js
+++ .scripts/formatJSON5docs.js
@@ -7,19 +7,21 @@ const validate = cmd.includes('--validate');
 
 const json5files = glob.sync('./**/*.json5');
 let counter = 0;
-json5files.forEach((fileName) => {
+json5files.forEach(fileName => {
   const content = fs.readFileSync(fileName).toString();
   const data = json5.parse(content);
   const newContent = json5.stringify(data, null, 2);
   if (newContent !== content) {
-    if(!validate) {
-       fs.writeFileSync(fileName, newContent);
+    if (!validate) {
+      fs.writeFileSync(fileName, newContent);
     }
-    counter ++;
+    counter++;
   }
 });
 
-console.log(`${counter} json5-file(s) ${validate ? 'should be' : ''} reformatted`);
+console.log(
+  `${counter} json5-file(s) ${validate ? 'should be' : ''} reformatted`,
+);
 
 if (validate && counter) {
   process.exit(1);
